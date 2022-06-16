dceb546be7a785203ef032a5551548bd00a620f3
zenox <hezhao@dianhun.cn>
2022-06-16 10:02:17 +0800

    重构自动解锁违规用户定时任务

--- src/main/java/org/seat/services/ViolationService.java
+++ src/main/java/org/seat/services/ViolationService.java
@@ -3,14 +3,13 @@ package org.seat.services;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import org.seat.beans.User;
-import org.seat.beans.UserExample;
 import org.seat.beans.Violation;
+import org.seat.mappers.UserMapper;
 import org.seat.mappers.ViolationMapper;
 import org.seat.utils.PageUtils;
-import org.seat.utils.UserPage;
 import org.seat.utils.ViolationPage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
@@ -18,36 +17,47 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Transactional(propagation = Propagation.REQUIRED,isolation = Isolation.DEFAULT)
+@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.DEFAULT)
 @Service
 public class ViolationService {
     @Autowired
-    ViolationMapper violationMapper;
-    public ViolationPage getViolationPage(int pageNum){
-        ViolationPage violationPage=new ViolationPage();
-        Page<Violation> page= PageHelper.startPage(pageNum, PageUtils.pageSize);
+    private ViolationMapper violationMapper;
+    @Autowired
+    private UserMapper userMapper;
+
+    public ViolationPage getViolationPage(int pageNum) {
+        ViolationPage violationPage = new ViolationPage();
+        Page<Violation> page = PageHelper.startPage(pageNum, PageUtils.pageSize);
         page.setReasonable(true);
-        List<Violation> violations=violationMapper.getAllViolationRecords();
-        PageInfo<Violation> pageInfo=new PageInfo<Violation>(violations,PageUtils.pageListSize);
+        List<Violation> violations = violationMapper.getAllViolationRecords();
+        PageInfo<Violation> pageInfo = new PageInfo<Violation>(violations, PageUtils.pageListSize);
         violationPage.setViolationList(violations);
         violationPage.setPageNum(pageNum);
         violationPage.setPageList(PageUtils.parsePageList(pageInfo.getNavigatepageNums()));
         violationPage.setPageCount(pageInfo.getPages());
         return violationPage;
     }
-    public boolean deleteViolation(int vid){
-        return violationMapper.deleteViolation(vid)>0;
+
+    public boolean deleteViolation(int vid) {
+        return violationMapper.deleteViolation(vid) > 0;
     }
-    public ViolationPage getViolationPageByUid(int pageNum,int uid){
-        ViolationPage violationPage=new ViolationPage();
-        Page<Violation> page= PageHelper.startPage(pageNum, PageUtils.pageSize);
+
+    public ViolationPage getViolationPageByUid(int pageNum, int uid) {
+        ViolationPage violationPage = new ViolationPage();
+        Page<Violation> page = PageHelper.startPage(pageNum, PageUtils.pageSize);
         page.setReasonable(true);
-        List<Violation> violations=violationMapper.getAllViolationRecordsByUid(uid);
-        PageInfo<Violation> pageInfo=new PageInfo<Violation>(violations,PageUtils.pageListSize);
+        List<Violation> violations = violationMapper.getAllViolationRecordsByUid(uid);
+        PageInfo<Violation> pageInfo = new PageInfo<Violation>(violations, PageUtils.pageListSize);
         violationPage.setViolationList(violations);
         violationPage.setPageNum(pageNum);
         violationPage.setPageList(PageUtils.parsePageList(pageInfo.getNavigatepageNums()));
         violationPage.setPageCount(pageInfo.getPages());
         return violationPage;
+    }
+
+    //定时任务解冻违规用户
+    @Scheduled(fixedRate = 60000)
+    public void scheduledUnlockAllViolationUser() {
+        userMapper.unlockDisabledUsers();
     }
 }
